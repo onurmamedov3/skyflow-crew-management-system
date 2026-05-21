9c0da532ea406411edd3f72287e7c3c1b8a70c1e
Mammadzada Onur Mahammad <onurmamedov1@gmail.com>
2026-05-21 12:40:15 +0400

    Initialized the project

---
+++ src/main/java/az/azal/skyflow/SkyflowApplication.java
@@ -0,0 +1,13 @@
+package az.azal.skyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyflowApplication.class, args);
+	}
+
+}
